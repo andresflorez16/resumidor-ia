720f26acae179699d20cd5d5546dc429943d6c69
andresflorez16 <andresflorez.dev@gmail.com>
2023-04-29 12:12:35 -0500

    add blur

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,13 @@ export default function Header () {
   return (
     <Box
       w='100%'
+      bg='#1a202c11'
+      backdropFilter='blur(5px)'
+      zIndex={10}
       textAlign='center'
       position='fixed'
       top={0}
-      mt={5}
+      p={3}
     >
       <Text fontSize='4xl' className={inter.className}>Resumidor IA</Text>
     </Box>
